77451a0e9c80bc82efdbb0f8bb4eb028d067030b
Seiji <upbook26@yahoo.co.jp>
2017-09-14 08:59:21 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,13 @@
 class TasksController < ApplicationController
+before_action :require_user_logged_in
+before_action :correct_user, only: [:edit,:update,:show,:destroy]
+
 def index
   @tasks = Task.all
 end
 
   def show
-    set_task
+    @user = current_user
   end
 
   def new
@@ -25,13 +28,9 @@ end
 
 
   def edit
-    set_task
   end
 
   def update
-        set_task
-        
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -42,21 +41,22 @@ end
   end
 
   def destroy
-      set_task
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
  private
- 
- def set_task
-    @task = Task.find(params[:id])
- end
 
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
-
